c845c6e0a8867cd88401e86105d6cf7a93e379e4
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-05-12 18:56:58 +0200

    add server.js and db.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
