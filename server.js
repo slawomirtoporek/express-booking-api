5a925069fb93a8d601ec72e9578864d2b981b7e8
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-05-17 20:14:00 +0200

    add build application

--- server.js
+++ server.js
@@ -15,13 +15,14 @@ app.use('/api', testmonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).json('Not found...');
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
-});
 
 app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
