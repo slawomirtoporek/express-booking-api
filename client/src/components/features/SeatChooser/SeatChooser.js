68790cd282db2d3befbcfaa3a6dfe3e1b2945680
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-05-24 23:55:57 +0200

    add the number of free chairs

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -11,6 +11,7 @@ const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
   const seats = useSelector(getSeats);
   const requests = useSelector(getRequests);
   const [socket, setSocket] = useState(null);
+  const [freeSeats, setFreeSeats] = useState();
 
   useEffect(() => {
     const socket = io(process.env.NODE_ENV === 'production' ? '' : 'ws://localhost:8000', { transports: ['websocket'] });
@@ -24,6 +25,14 @@ const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
       socket.disconnect();
     };
   }, [dispatch]);
+
+  useEffect( () => {
+    setFreeSeats( () => {
+    const occupiedSeats = seats.filter(seat => seat.day === chosenDay);
+    const emptySeats = 50 - occupiedSeats.length;
+    return emptySeats;
+    });
+  ;}, [chosenDay, seats]);
 
   const isTaken = (seatId) => {
     return (seats.some(item => (item.seat === seatId && item.day === chosenDay)));
@@ -45,6 +54,7 @@ const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
       { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i+1) )}</div>}
       { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={50} /> }
       { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error) && <Alert color="warning">Couldn't load seats...</Alert> }
+      <p>Free seats: {freeSeats}/50</p>
     </div>
   )
 }
