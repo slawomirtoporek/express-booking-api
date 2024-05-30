489b5c9f1544c9cff5dfcabd0dedb84f99020015
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-05-30 21:10:22 +0200

    change concerts endpoints use concert.model mongoose

--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -3,93 +3,124 @@ const db = require('../db/db');
 const uuid = require('uuid');
 const multer = require('multer');
 const path = require('path');
+const Concert = require('../models/concert.model');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'img/uploads/');
+    cb(null, 'public/img/uploads');
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    const ext = path.extname(file.originalname);
+    const nameuuid = uuid.v4();
+    const name = nameuuid.replaceAll('-', '');
+    cb(null, `${name}${ext}`);
   }
 });
+
+const app = express();
+const upload = multer({ storage: storage });
+const router = express.Router();
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 const pathReplace = path => {
   const newpath = path.replaceAll('\\', '/');
   return newpath;
 };
 
-const app = express();
-const upload = multer({ storage: storage });
-const router = express.Router();
-
-app.use(express.static(path.join(__dirname, '/img')));
-
-router.route('/concerts').get((req, res) => {
-  if (db.concerts.length === 0) {
-    return res.status(404).json({ message: 'No concerts found' });
-  } else {
-    res.json(db.concerts);
-  };
-});
-
-router.route('/concerts/:id').get((req, res) => {
-  const id = req.params.id;
-  const concert = db.concerts.find(data => data.id.toString() === String(id));
-  if (!concert) {
-    res.status(404).json({ message: 'Concert not found' });
-  } else {
-    res.json(concert);
-  };
-});
-
-router.route('/concerts').post(upload.single('image'), (req, res) => {
-  const id = uuid.v4();
-  const { performer, genre, price, day } = req.body;
-  const image = req.file;
-  const newpath = pathReplace(image.path);
-  if (!isNaN(price) && !isNaN(day)) {
-    if (performer && genre && price && day && image) {
-      db.concerts.push({ id, performer, genre, price, day, image: newpath });
-      res.json({ message: 'OK' });
+router.get('/concerts', async (req, res) => {
+  try {
+    const concert = await Concert.find();
+    if (concert.length === 0) {
+      return res.status(404).json({ message: 'No concerts found' });
     } else {
-      res.status(400).json({ message: 'All fields are required' });
-    }
-  } else {
-    res.status(400).json({ message: 'Price and day must be numbers' });
-  };
+      res.json(concert);
+    };
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
 });
 
-router.route('/concerts/:id').delete((req, res) => {
-  const id = req.params.id;
-  const index = db.concerts.findIndex(data =>  data.id.toString() === String(id));
-  if (index !== -1) {
-    db.concerts = db.concerts.filter(data => data.id.toString() !== String(id));
-    res.json({ message: 'OK' });
-  } else {
-    res.status(404).json({ message: 'Concerts not found' });
-  };
+router.get('/concerts/:id', async (req, res) => {
+  try {
+    const concert = await Concert.findById(req.params.id);
+    if (!concert) {
+      res.status(404).json({ message: 'Concert not found' });
+    } else {
+      res.json(concert);
+    };
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
 });
 
-router.route('/concerts/:id').put(upload.single('image'), (req, res) => {
-  const id = req.params.id;
+router.post('/concerts', upload.single('image'), async (req, res) => {
   const { performer, genre, price, day } = req.body;
-  const image = req.file;
-  const newpath = pathReplace(image.path);
-  const index = db.concerts.findIndex(data =>  data.id.toString() === String(id));
-  if (index !== -1) {
+
+  try {
+    const image = req.file;
+    const newpath = pathReplace(image.path);
     if (!isNaN(price) && !isNaN(day)) {
       if (performer && genre && price && day && image) {
-        db.concerts[index] = { ...db.concerts[index], performer, genre, price, day, image: newpath };
+        const newConcert = new Concert({ performer: performer, genre: genre, 
+          price: price, day: day, image: newpath });
+        await newConcert.save();
         res.json({ message: 'OK' });
       } else {
-        res.status(404).json({ message: 'All fields are required' });
-      };
+        res.status(400).json({ message: 'All fields are required' });
+      }
     } else {
       res.status(400).json({ message: 'Price and day must be numbers' });
     };
-  } else {
-    res.status(404).json({ message: 'Concert not found' });
-  };
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
+});
+
+router.delete('/concerts/:id', async (req, res) => {
+  try {
+    const concert = await Concert.findById(req.params.id);
+    if (concert) {
+      await Concert.deleteOne(concert);
+      res.json(concert);
+    } else {
+      res.status(404).json({ message: 'Concerts not found' });
+    };
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
+});
+
+router.put('/concerts/:id', upload.single('image'), async (req, res) => {
+  const { performer, genre, price, day } = req.body;
+  
+  try {
+    const image = req.file;
+    const newpath = pathReplace(image.path);
+    const concert = await Concert.findById(req.params.id);
+    if (concert) {
+      if (!isNaN(price) && !isNaN(day)) {
+        if (performer && genre && price && day && image) {
+          await Concert.updateOne({ _id: req.params.id }, 
+            {$set: { performer: performer, genre: genre, price: price, day: day, image: newpath }});
+          res.json(concert);
+        } else {
+          res.status(404).json({ message: 'All fields are required' });
+        };
+      } else {
+        res.status(400).json({ message: 'Price and day must be numbers' });
+      };
+    } else {
+      res.status(404).json({ message: 'Concert not found' });
+    };
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
 });
 
 module.exports = router;
